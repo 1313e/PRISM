7eacbf0148db09f8a2fd847104468b9c5beb1fa8
1313e <ellert_vandervelden@outlook.com>
2018-10-01 18:53:21 +1000

    Importing PRISM in MPI (with size > 1) with the environment variable 'OMP_NUM_THREADS' not set to 1, now raises a warning.

--- prism/__init__.py
+++ prism/__init__.py
@@ -30,6 +30,10 @@ the *PRISM* pipeline and holds all information about this model.
 # Future imports
 from __future__ import absolute_import, division, print_function
 
+# Built-in imports
+import os
+import warnings
+
 # Import PRISM modules
 from .__version__ import prism_version as __version__
 from . import _internal
@@ -45,4 +49,20 @@ __author__ = "Ellert van der Velden"
 
 
 # %% EXECUTE INITIALIZING CODE
+# Import PRISM's custom cmaps
 _internal.import_cmaps()
+
+# Check if MPI is being used
+try:
+    from mpi4py import MPI
+except ImportError:
+    pass
+else:
+    # If so, raise warning if OMP_NUM_THREADS is not set to 1 with MPI_size > 1
+    if(os.environ.get('OMP_NUM_THREADS') != '1' and
+       MPI.COMM_WORLD.Get_size() > 1 and MPI.COMM_WORLD.Get_rank() == 0):
+        warn_msg = ("Environment variable 'OMP_NUM_THREADS' is currently not "
+                    "set to 1 (%s), with MPI enabled. Unless this was "
+                    "intentional, it is advised to set 'OMP_NUM_THREADS' to 1."
+                    % (os.environ.get('OMP_NUM_THREADS')))
+        warnings.warn(warn_msg)
