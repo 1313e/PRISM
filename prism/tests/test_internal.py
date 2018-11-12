Renamed 'is_listening' to 'worker_mode'. Fixed a bug where the check_vals()-function could sometimes give incorrect results when used on Windows.

prism/tests/test_internal.py
@@ -8,7 +8,7 @@ from __future__ import (absolute_import, division, print_function,
 # Built-in imports
 import logging
 from os import path
-from sys import version_info
+from sys import platform, version_info
 
 # Package imports
 from e13tools.core import InputError
@@ -29,6 +29,9 @@ dirpath = path.dirname(__file__)
 
 # Save major version
 vmajor = version_info.major
+
+# Save if this platform is Windows
+win32 = platform.startswith('win')
 
 
 # %% PYTEST CLASSES AND FUNCTIONS
@@ -242,7 +245,7 @@ class Test_check_val(object):
         array = np.array([False, True])
         array2 = check_vals(array, 'array', 'bool')
         assert (array2 == [0, 1]).all()
-        assert array2.dtype.name == 'int64'
+        assert array2.dtype.name == 'int64' if not win32 else 'int32'
 
         # Check for NumPy array of strings
         array = np.array(['a', 'b'])

prism/tests/test_pipeline_emulator_projection.py
@@ -247,12 +247,12 @@ class Test_Pipeline_Gaussian2D(object):
         pipe.do_logging = 1
 
     # Check if the workers can start listening
-    def test_workers_listening(self, pipe):
-        pipe.is_listening = False
+    def test_worker_mode(self, pipe):
+        pipe.worker_mode = False
         pipe._make_call('details')
-        pipe.is_listening = True
+        pipe.worker_mode = True
         pipe._make_call(np.array, [1])
-        pipe.is_listening = False
+        pipe.worker_mode = False
 
 
 # Pytest for standard Pipeline class (+Emulator, +Projection) for 3D model