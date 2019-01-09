412f22c79c8c0a8baf03012031c42dbd7dfddf72
Ellert van der Velden <ellert_vandervelden@outlook.com>
2019-01-09 12:37:30 +1100

    Slight change to the imports to use the full import path.

--- prism/modellink/_sine_wave_link.py
+++ prism/modellink/_sine_wave_link.py
@@ -19,7 +19,7 @@ from __future__ import absolute_import, division, print_function
 import numpy as np
 
 # PRISM imports
-from prism.modellink import ModelLink
+from prism.modellink._modellink import ModelLink
 
 # All declaration
 __all__ = ['SineWaveLink']
