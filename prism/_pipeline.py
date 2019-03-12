c3f9500568888c69c1a178db3a99173c338bcaa4
1313e <ellert_vandervelden@outlook.com>
2019-03-12 22:19:47 +1100

    Changed docstring of the evaluate()-method of the Pipeline class to be more clear about when it returns and when it prints.

--- prism/_pipeline.py
+++ prism/_pipeline.py
@@ -2965,8 +2965,8 @@ class Pipeline(Projection, object):
         --------
         %(emul_i)s
 
-        Returns (if ndim(sam_set) > 1)
-        ------------------------------
+        Returns (if 2D `sam_set`)
+        -------------------------
         impl_check : list of bool
             List of bool indicating whether or not the given samples passed the
             implausibility check at the given emulator iteration `emul_i`.
@@ -2983,8 +2983,8 @@ class Pipeline(Projection, object):
             Array containing the univariate implausibility values for all given
             samples.
 
-        Prints (if ndim(sam_set) == 1)
-        ------------------------------
+        Prints (if 1D `sam_set`)
+        ------------------------
         impl_check : bool
             Bool indicating whether or not the given sample passed the
             implausibility check at the given emulator iteration `emul_i`.
