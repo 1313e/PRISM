661e9cee4e9f32f3953f5ce61d0dd628536b8177
Ellert van der Velden <ellert_vandervelden@outlook.com>
2019-06-17 16:52:43 +1000

    Set version to 1.1.2, as this is going to be the new release.

--- prism/__version__.py
+++ prism/__version__.py
@@ -10,7 +10,7 @@ Stores the different versions of the *PRISM* package.
 
 # %% VERSIONS
 # Default/Latest/Current version
-__version__ = '1.1.2.dev1'
+__version__ = '1.1.2'
 
 # Compatibility versions
 compat_version = ['1.0.0rc12']
