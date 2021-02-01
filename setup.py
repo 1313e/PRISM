a5ebd69307270192b70427b203dff4ab2c3d4a60
1313e <ellert_vandervelden@outlook.com>
2021-02-01 14:55:56 +1100

    Do not state that PRISM is compatible with Python 3.9.

--- setup.py
+++ setup.py
@@ -71,7 +71,6 @@ setup(name='prism',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
           'Topic :: Scientific/Engineering',
           'Topic :: Software Development'
           ],
